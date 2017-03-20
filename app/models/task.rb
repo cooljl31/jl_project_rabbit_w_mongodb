f927ed4bdd9bb51daa8fb7e2611f3c7c52cc189a
jim lesperance <contact@jimlesperance.com>
2017-03-20 22:51:13 +0100

    add category

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,11 @@
 class Task
   include Mongoid::Document
+  include Mongoid::Timestamps
   field :title, type: String
   field :description, type: String
   field :company, type: String
   field :url, type: String
+
+  field :category_id, type: Integer
+  belongs_to :category
 end
